bf19c9595fd86f4d5402d37ed90d99717d07e52e
films1 <50090988+films1@users.noreply.github.com>
2019-04-30 09:51:27 +0700

    Add files via upload

---
+++ firstbot test 1.php
@@ -0,0 +1,48 @@
+<?php
+function reply_msg($txtin,$replyToken)//สร้างข้อความและตอบกลับ
+{
+    $access_token = "DhLf3HvnUjaa+/bPzmlDK1w+Zvg5sP4upcpq5RsY/l7kuBM75z5+2nbcpsF+sI8V4cQkWu76aVd/ofL85XEg7hfektWlNuhlasQCmfCdoDd0c/yQX8s9OobLNV1gF6HDTzTQqh8O6UIMVQZtXuICSAdB04t89/1O/w1cDnyilFU=";
+    $messages = ['type' => 'text','text' => $txtin];//สร้างตัวแปร 
+    $url = 'https://api.line.me/v2/bot/message/reply';
+    $data = [
+                'replyToken' => $replyToken,
+                'messages' => [$messages],
+            ];
+    $post = json_encode($data);
+    $headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+    $ch = curl_init($url);
+    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+    $result = curl_exec($ch);
+    curl_close($ch);
+    echo $result . "\r\n";
+}
+
+// รับข้อมูล
+require('films1.php');
+
+$content = file_get_contents('php://input');//รับข้อมูลจากไลน์
+$events = json_decode($content, true);//แปลง json เป็น php
+file_put_contents('log.txt',$events,FILE_APPEND); //สร้างไฟล์ log
+if (!is_null($events['events'])) //check ค่าในตัวแปร $events
+{
+    foreach ($events['events'] as $event) {
+        if ($event['type'] == 'message' && $event['message']['type'] == 'text')
+        {
+            $replyToken = $event['replyToken']; //เก็บ reply token เอาไว้ตอบกลับ
+            $source_type = $event['source']['type'];//เก็บที่มาของ event(user หรือ group)
+            $txtin = $event['message']['text'];//เอาข้อความจากไลน์ใส่ตัวแปร $txtin
+            $sql_text = "SELECT * FROM films1 WHERE equip_id LIKE '%$txtin%'";
+            $query = mysqli_query($sql_text);
+            while($obj = mysqli_fetch_assoc($qoery))
+            {
+               $text_back = $txt_back."\n".$obj["equip_name"]
+            }
+            reply_msg($txtback,$replyToken);      
+        }
+    }
+}
+echo "BOT OK";
